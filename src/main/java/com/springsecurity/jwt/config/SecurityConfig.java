a0f1c0a206b2674cc843c423193c9545c66254ee
Tanguri <jungwontwo2@naver.com>
2024-05-23 15:15:06 +0900

    db기반 로그인 검증 로직

--- src/main/java/com/springsecurity/jwt/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/jwt/config/SecurityConfig.java
@@ -16,39 +16,52 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
-    public SecurityConfig(AuthenticationConfiguration authenticationManager){
-        this.authenticationConfiguration=authenticationManager;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception{
-        return configuration.getAuthenticationManager();
-    }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 
-        http.csrf((auth)->auth.disable());
+        http
+                .csrf((auth) -> auth.disable());
 
-        http.formLogin((auth) -> auth.disable());
+        http
+                .formLogin((auth) -> auth.disable());
 
-        http.httpBasic((auth) -> auth.disable());
+        http
+                .httpBasic((auth) -> auth.disable());
 
-        http.authorizeHttpRequests((auth)->auth
-                .requestMatchers("/login","/","/join").permitAll()
-                .requestMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated());
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .anyRequest().authenticated());
 
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
-        http.sessionManagement((session)->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
