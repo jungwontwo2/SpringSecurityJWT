c3edb7b2685bc7e3e44bc18eb5633a25ae6af6d0
Tanguri <jungwontwo2@naver.com>
2024-05-23 09:18:48 +0900

    회원가입

--- src/main/java/com/springsecurity/jwt/repository/UserRepository.java
+++ src/main/java/com/springsecurity/jwt/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.springsecurity.jwt.repository;
 import com.springsecurity.jwt.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity,Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
 }
